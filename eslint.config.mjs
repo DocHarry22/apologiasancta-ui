ab8955fc2b38780d1ba32852deb46e9570e89ed1
DocHarry <thletsholo2@gmail.com>
2026-05-24 15:56:13 +0200

    Fix UI CI lint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,12 +5,21 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      "react-hooks/set-state-in-effect": "off",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
+    "android/.gradle/**",
+    "android/build/**",
+    "android/app/build/**",
+    "**/*-Water-Nest-HP002*",
     "next-env.d.ts",
   ]),
 ]);
